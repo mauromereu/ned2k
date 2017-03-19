a9204f67e3d9c41628b2cc793a473faf5e47af5c
zyEros <395693101@qq.com>
2017-03-19 22:55:31 +0800

    ServerConn add offerfile and search method

--- test/ServerConn.js
+++ test/ServerConn.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const ServerConn = require('../lib/ServerConn');
+const Constant = require('../lib/Constant');
 
 const SERVER_HOST = '222.40.142.3';
 const SERVER_PORT = 40072;
@@ -55,6 +56,32 @@ describe('ServerConn', () => {
   it('#server ident', (done) => {
     server.on('package:serverIdent', (info) => {
       info && done();
+    });
+  });
+
+  it('#offer files', (done) => {
+    server.on('ready', () => {
+      server.offerFiles([{
+        hash: '5d430a6c2d0ea11999f6eafff0996fdd',
+        ip: 0x0,
+        port: 4662,
+        name: 'filename',
+        size: 1024 * 1024 * 10,
+        type: Constant.FILETYPE.IMAGE
+      }]);
+    });
+  });
+
+  it('#search', (done) => {
+    server.on('ready', () => {
+      server.search({
+        originString: '(java OR nodejs) AND (php NOT it)',
+        type: Constant.FILETYPE.VIDEO,
+        minSize: 1024 * 1024,
+        maxSize: 10 * 1024 * 1024,
+        sourceNum: 10,
+        ext: 'jpg'
+      });
     });
   });
 });
